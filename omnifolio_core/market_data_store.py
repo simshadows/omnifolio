aa408ac5637cb197e8233ccc841e10ed4383925e
simshadows <contact@simshadows.com>
2021-02-08 21:59:10 +1100

    INSERT queries are now only done on dates missing from the database.
    We still need to store events.

--- omnifolio_core/market_data_store.py
+++ omnifolio_core/market_data_store.py
@@ -52,11 +52,11 @@ class MarketDataStore:
                 CREATE TABLE stock_timeseries_daily_prices (
                     symbol               TEXT,
                     exchange             TEXT,
-                    date                 INTEGER, -- Unix Time
+                    date                 DATE,
 
                     data_source          TEXT,
                     data_trust_value     INTEGER,
-                    data_collection_time INTEGER, -- Unix Time
+                    data_collection_time DATETIME,
 
                     open                 INTEGER,
                     high                 INTEGER,
@@ -79,11 +79,11 @@ class MarketDataStore:
                 CREATE TABLE stock_timeseries_daily_exdividend_events (
                     symbol               TEXT,
                     exchange             TEXT,
-                    date                 INTEGER, -- Unix Time
+                    date                 DATE,
 
                     data_source          TEXT,
                     data_trust_value     INTEGER,
-                    data_collection_time INTEGER, -- Unix Time
+                    data_collection_time DATETIME,
 
                     dividend_per_unit    INTEGER,
 
@@ -100,11 +100,11 @@ class MarketDataStore:
                 CREATE TABLE stock_timeseries_daily_split_events (
                     symbol               TEXT,
                     exchange             TEXT,
-                    date                 INTEGER, -- Unix Time
+                    date                 DATE,
 
                     data_source          TEXT,
                     data_trust_value     INTEGER,
-                    data_collection_time INTEGER, -- Unix Time
+                    data_collection_time DATETIME,
 
                     -- split_numerator and split_denominator represent the split value as a fraction.
                     -- Pre-split units get multiplied by this split value to get post-split units.
@@ -130,12 +130,78 @@ class MarketDataStore:
                 self._market_data_store_path,
                 self._STOCK_TIMESERIES_DAILY__FILEPATH,
             )
-        with sqlite3.connect(path) as conn:
+        with sqlite3.connect(path, detect_types=sqlite3.PARSE_DECLTYPES) as conn:
             if not self._check_if_db_is_set_up(conn):
                 self._set_up_db(conn)
                 if not self._check_if_db_is_set_up(conn):
                     raise RuntimError("Database schema check failed, even after setting it up.")
             yield conn
+
+    @staticmethod
+    def _get_price_data_dates_as_set(conn, *, symbol, exchange, provider):
+        query = """
+                SELECT date [date]
+                FROM stock_timeseries_daily_prices
+                WHERE
+                    symbol = ?
+                    AND exchange = ?
+                    AND data_source = ?;
+            """
+        cursor = conn.execute(query, (symbol, exchange, provider))
+        ret = {x[0] for x in cursor}
+        assert all(isinstance(x, datetime.date) for x in ret)
+        return ret
+
+    @staticmethod
+    def _add_price_data(conn, *, symbol, exchange, date, day_data):
+        assert isinstance(symbol, str)
+        assert isinstance(exchange, str)
+        assert isinstance(date, datetime.date)
+        assert isinstance(day_data, DayPrices)
+        query = """
+                INSERT INTO stock_timeseries_daily_prices VALUES (
+                    ?, -- symbol
+                    ?, -- exchange
+                    ?, -- date
+
+                    ?, -- data_source
+                    ?, -- data_trust_value
+                    ?, -- data_collection_time
+
+                    ?, -- open
+                    ?, -- high
+                    ?, -- low
+                    ?, -- close
+                    ?, -- adjusted_close
+
+                    ?, -- volume
+
+                    ?, -- unit
+                    ?  -- price_denominator
+                );
+            """
+        params = (
+                symbol,
+                exchange,
+                date,
+
+                day_data.data_source,
+                day_data.data_trust_value,
+                day_data.data_collection_time,
+
+                day_data.open,
+                day_data.high,
+                day_data.low,
+                day_data.close,
+                day_data.adjusted_close,
+
+                day_data.volume,
+
+                day_data.unit,
+                day_data.price_denominator,
+            )
+        conn.execute(query, params)
+        return
 
     ######################################################################################
 
@@ -158,59 +224,27 @@ class MarketDataStore:
                 assert isinstance(provider_name, str) and (len(provider_name.strip()) > 0)
                 assert isinstance(provider_data, StockTimeSeriesDailyResult)
 
-                # We first check the latest dates.
-                # TODO: Implement this check. We're skipping it for now.
+                prices = provider_data.prices
 
-                for (date, day_data) in provider_data.prices.items():
+                pulled_dates = set(prices.keys())
+                stored_dates = self._get_price_data_dates_as_set(
+                        conn,
+                        symbol=symbol,
+                        exchange="PLACEHOLDER",
+                        provider=provider_name,
+                    )
+                dates_missing_in_store = pulled_dates - stored_dates
 
-                    assert isinstance(date, datetime.date)
-                    assert isinstance(day_data, DayPrices)
+                for date in dates_missing_in_store:
+                    day_data = provider_data.prices[date]
 
-                    query = """
-                            INSERT INTO stock_timeseries_daily_prices VALUES (
-                                ?, -- symbol
-                                ?, -- exchange
-                                ?, -- date
-
-                                ?, -- data_source
-                                ?, -- data_trust_value
-                                ?, -- data_collection_time
-
-                                ?, -- open
-                                ?, -- high
-                                ?, -- low
-                                ?, -- close
-                                ?, -- adjusted_close
-
-                                ?, -- volume
-
-                                ?, -- unit
-                                ?  -- price_denominator
-                            );
-                        """
-                    params = (
-                            symbol,
-                            "PLACEHOLDER",
-                            date,
-
-                            day_data.data_source,
-                            day_data.data_trust_value,
-                            day_data.data_collection_time,
-
-                            day_data.open,
-                            day_data.high,
-                            day_data.low,
-                            day_data.close,
-                            day_data.adjusted_close,
-
-                            day_data.volume,
-
-                            day_data.unit,
-                            day_data.price_denominator,
+                    self._add_price_data(
+                            conn,
+                            symbol=symbol,
+                            exchange="PLACEHOLDER",
+                            date=date,
+                            day_data=day_data,
                         )
-                    conn.execute(query, params)
-
-                    # TODO: Type check day_data?
 
                 # TODO: Events!
 
